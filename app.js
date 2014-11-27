d6473fd2e61c897d899c216b7292ad01cbdf7d90
gonzalowtf <gonzalowtf@gmail.com>
2014-11-27 16:11:04 -0300

    listing and chating with all users , logi and log out

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express"),
 	app= express(),
 	server = require("http").createServer(app),
-	io = require("socket.io").listen(server);
+	io = require("socket.io").listen(server),
+	nicknames = [];
 
 // server where the app will be listening
 
@@ -20,10 +21,36 @@ app.get('/', function(req, res){
 
 // when the connection is ready everywhere here appears this
 io.sockets.on("connection",function(socket){
+
+// for the online users ta the moment od connection
+	socket.on("new-user",function(data, callback){
+
+       if(nicknames.indexOf(data) != -1){
+       	callback(false);
+       }
+       else{
+       	callback(true);
+       	socket.nickname=data;
+       	nicknames.push(socket.nickname);
+       	updatenicknames();
+       }
+
+	});
     //"send-mesage" and "new message" are the functions on index and works everywhere
     socket.on("send-message" , function(data){
-    	io.sockets.emit('new-message',data);
+    	io.sockets.emit('new-message',{msg:data , nick : socket.nickname});
     });
 
+function updatenicknames(){
+	       	io.sockets.emit("usernames",nicknames);
 
+
+}
+
+  socket.on("disconnect" , function(data){
+
+  			if(!socket.nickname) return;
+  			nicknames.splice(nicknames.indexOf(socket.nickname),1);
+  			updatenicknames();
+  });
 });
