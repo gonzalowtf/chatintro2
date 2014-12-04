ea81c2ba3902d4c43a1431c2934136643ca4f6e2
gonzalowtf <gonzalowtf@gmail.com>
2014-12-04 16:00:44 -0300

    first one

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express"),
 	app= express(),
 	server = require("http").createServer(app),
 	io = require("socket.io").listen(server),
-	nicknames = [];
+	users = {};
 
 // server where the app will be listening
 
@@ -26,24 +26,49 @@ io.sockets.on("connection",function(socket){
 // for the online users ta the moment od connection
 	socket.on("new-user",function(data, callback){
 
-       if(nicknames.indexOf(data) != -1){
+       if(data in users){
        	callback(false);
        }
        else{
        	callback(true);
        	socket.nickname=data;
-       	nicknames.push(socket.nickname);
+        users[socket.nickname] = socket;
+       	//nicknames.push(socket.nickname);
        	updatenicknames();
        }
 
 	});
     //"send-mesage" and "new message" are the functions on index and works everywhere
-    socket.on("send-message" , function(data){
-    	io.sockets.emit('new-message',{msg:data , nick : socket.nickname});
+    socket.on("send-message" , function(data,callback){
+      var msg = data.trim(); //this take care of a posible space at the beggining
+      if(msg.substr(0,3) === '/w '){         // (0,3) first three characters of the word
+
+                msg= msg.substr(3);
+                var ind = msg.indexOf(" ");
+                if(ind != -1){
+                  var name = msg.substring(0,ind);
+                  var msg = msg.substring(ind + 1);
+                  if(name in users){
+                    users[name].emit('whisper',{msg:msg , nick : socket.nickname});
+
+                 console.log("whisper");
+               }else{
+                    callback("Error : enter valid user");
+               }
+               }else{
+
+                callback("Error , please enter a messege for your whisper");
+
+               }
+
+
+      }else{
+    	io.sockets.emit('new-message',{msg:msg , nick : socket.nickname});
+    }
     });
 
 function updatenicknames(){
-	       	io.sockets.emit("usernames",nicknames);
+	       	io.sockets.emit("usernames",Object.keys(users));
 
 
 }
@@ -51,7 +76,7 @@ function updatenicknames(){
   socket.on("disconnect" , function(data){
 
   			if(!socket.nickname) return;
-  			nicknames.splice(nicknames.indexOf(socket.nickname),1);
+        delete users[socket.nickname];
   			updatenicknames();
   });
 });
